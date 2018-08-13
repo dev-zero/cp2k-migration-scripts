0983b3bbe36d2d03b6cc77c74b748007c7615a5b
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-08-13 15:16:46 +0200

    push: use --force to overwrite previous trial

--- migration-03-push.sh
+++ migration-03-push.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 
 git --git-dir=cp2k-repo/.git remote add origin git@github.com:dev-zero/migrated-cp2k.git
-git --git-dir=cp2k-repo/.git push --all --follow-tags --set-upstream origin
+git --git-dir=cp2k-repo/.git push --all --follow-tags --set-upstream origin --force
 
 git --git-dir=cp2k-data-repo/.git remote add origin git@github.com:dev-zero/migrated-cp2k-data.git
-git --git-dir=cp2k-data-repo/.git push --all --follow-tags --set-upstream origin
+git --git-dir=cp2k-data-repo/.git push --all --follow-tags --set-upstream origin --force
