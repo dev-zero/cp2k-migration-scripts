bb99b967b8f42c286145e8c2032026b5a308c4fb
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-10-02 10:05:46 +0200

    push: switch to final repo URLs

--- migration-04-push.sh
+++ migration-04-push.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 
 
-git --git-dir=cp2k-repo/.git remote add origin git@github.com:dev-zero/migrated-cp2k.git
+git --git-dir=cp2k-repo/.git remote add origin git@github.com:cp2k/cp2k.git
 git --git-dir=cp2k-repo/.git push --all --follow-tags --set-upstream origin --force
 
-git --git-dir=cp2k-data-repo/.git remote add origin git@github.com:dev-zero/migrated-cp2k-data.git
+git --git-dir=cp2k-data-repo/.git remote add origin git@github.com:cp2k/cp2k-data.git
 git --git-dir=cp2k-data-repo/.git push --all --follow-tags --set-upstream origin --force
